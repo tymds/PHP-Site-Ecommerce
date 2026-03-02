64079ab641b5eda52508da7d387dc10e01a20eb6
strongworldd <michotkevin271@gmail.com>
2026-03-02 17:19:54 +0100

    sup home

--- app/controllers/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-$authenticated = is_authenticated();
-$username = $authenticated ? (string)($_SESSION['username'] ?? '') : '';
-$email = $authenticated ? (string)($_SESSION['email'] ?? '') : '';
-$pageTitle = 'Shop | Home';
-
-require __DIR__ . '/../views/home.php';
